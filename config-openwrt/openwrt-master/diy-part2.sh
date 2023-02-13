892aa1c83a9f855622cf8b8d605a48a82a28682d
wisonic-s <shaochuan.wu@fit2cloud.com>
2023-02-13 11:49:09 +0800

    update

--- config-openwrt/openwrt-master/diy-part2.sh
+++ config-openwrt/openwrt-master/diy-part2.sh
@@ -44,6 +44,4 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # git apply ../config-openwrt/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
-wget https://github.com/vernesong/OpenClash/archive/master.zip
-unzip master.zip
-cp -r OpenClash-master/luci-app-openclash package
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
